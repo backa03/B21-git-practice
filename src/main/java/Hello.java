3f5914848bc9774e9e53de831e0c97ddd3f54862
backa03 <tamerlanbacev@gmail.com>
2021-01-26 15:37:49 -0500

    movie2 branck watchin om the phone

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World, B21");
+        System.out.println("Hello World, B22");
         //watching more movie in the bedroom
         //ready to go back
+        //movie on the phone 10 min
     }
 }
