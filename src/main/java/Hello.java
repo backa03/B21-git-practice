ec7722d3b8e2ca1ceed84f835a2551881dc2bbb8
backa03 <tamerlanbacev@gmail.com>
2021-01-26 15:23:53 -0500

    conflict1 more work more

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World, B21");
+        System.out.println("Hello World, B22");
         //watching more movie in the bedroom
     }
 }
